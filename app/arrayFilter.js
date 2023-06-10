724d3c82bbdafdb08f0952667e5e5b743a8679ae
livehass <silva.felipe12@hotmail.com>
2023-06-10 00:30:47 -0300

    adiciona arrafilter.js

--- app/arrayFilter.js
+++ app/arrayFilter.js
@@ -1,14 +1,14 @@
 const btnFiltrarLivrosFront = document.getElementById('btnFiltrarLivrosFront');
-btnFiltrarLivrosFront.addEventListener('click', filtarLivrosDeFront())
+btnFiltrarLivrosFront.addEventListener('click', filtarLivrosDeFront());
 
 const btnFiltrarLivrosBack = document.getElementById('btnFiltrarLivrosBack');
 const btnFiltrarLivrosDados = document.getElementById('btnFiltrarLivrosDados');
-const btnLivrosDisponiveis = document.getElementById('btnLivrosDisponiveis');
-const btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
+const btnL;ivrosDisponiveis = document.getElementById('btnLivrosDisponiveis');
+
 
 
 function filtarLivrosDeFront(){
-    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end' )
-    console.table(livrosFiltrados)
+    let livrosFiltrados = livros.filter(livro => livro.categoria == 'dados' )
+    console.log(livrosFiltrados)
 }
 
