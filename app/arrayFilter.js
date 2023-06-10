1ccd24c2f2c5c90d7b36bd116ce31b090b1cc8e5
livehass <silva.felipe12@hotmail.com>
2023-06-09 23:36:15 -0300

    adiciona arrafilter.js

--- app/arrayFilter.js
+++ app/arrayFilter.js
@@ -1,8 +1,12 @@
 const btnFiltrarLivrosFront = document.getElementById('btnFiltrarLivrosFront');
-btnFiltrarLivrosFront.addEventListener('click', ()=>{
+btnFiltrarLivrosFront.addEventListener('click', filtarLivrosDeFront())
 
-})
 const btnFiltrarLivrosBack = document.getElementById('btnFiltrarLivrosBack');
 const btnFiltrarLivrosDados = document.getElementById('btnFiltrarLivrosDados');
 const btnLivrosDisponiveis = document.getElementById('btnLivrosDisponiveis');
 const btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
+
+
+function filtarLivrosDeFront(){
+
+}
