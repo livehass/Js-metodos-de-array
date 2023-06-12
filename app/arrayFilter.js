94c34b0eebb3efaf5d0f8dcbf667f906cce2d216
livehass <silva.felipe12@hotmail.com>
2023-06-12 01:09:27 -0300

    refaz a função para ser mais dinamica e abrangente

--- app/arrayFilter.js
+++ app/arrayFilter.js
@@ -2,9 +2,6 @@ const botoes = document.querySelectorAll('.btn')
 botoes.forEach(btn => btn.addEventListener('click', filtarLivros))
  
 // console.table(botoes);
- 
-
-
 function filtarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
