2757d355a50940fc42f24de3d43fbe762f78f37d
livehass <silva.felipe12@hotmail.com>
2023-06-15 12:12:08 -0300

    adiciona função filtrar por disponibilidade

--- app/arrayFilter.js
+++ app/arrayFilter.js
@@ -5,7 +5,7 @@ botoes.forEach(btn => btn.addEventListener('click', filtarLivros))
 function filtarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : livros.filter(livro => livro.categoria == categoria);
     exibirOsLivrosNaTela(livrosFiltrados)
 
     if(categoria == 'disponivel'){
@@ -13,6 +13,10 @@ function filtarLivros(){
     }
   
 }
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
 function exibeValorTotalDosLivrosNaTela() {
     elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
     <div class="livros__disponiveis">
