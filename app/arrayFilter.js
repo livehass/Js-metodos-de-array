631fca9ff6976f72f70ad0860b35b0a922d1c701
livehass <silva.felipe12@hotmail.com>
2023-06-15 13:22:44 -0300

    adiciona novo array reduce

--- app/arrayFilter.js
+++ app/arrayFilter.js
@@ -5,6 +5,7 @@ botoes.forEach(btn => btn.addEventListener('click', filtarLivros))
 function filtarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
+    
     let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
     exibirOsLivrosNaTela(livrosFiltrados)
 
